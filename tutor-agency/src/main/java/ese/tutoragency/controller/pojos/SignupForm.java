1383217c5d0eeaa6cb003e16a95148ef2b9af31b
laeri <lars.wuethrich@hotmail.com>
2015-10-19 21:24:25 +0200

    Began working on use case "create user account" and added a signupform (based on skeleton), register.jsp (based on protoype).
    Autowiring didn't work, might be a problem in my setup. Please confirm, outcommented
    for the moment..

---
+++ tutor-agency/src/main/java/ese/tutoragency/controller/pojos/SignupForm.java
@@ -0,0 +1,78 @@
+package ese.tutoragency.controller.pojos;
+
+import javax.validation.constraints.Pattern;
+
+import com.sun.istack.internal.NotNull;
+
+public class SignupForm {
+
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String nickname;
+    private String password;
+    private String passwordConfirm;
+
+
+    @NotNull
+    @Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", 
+    message = "Must be valid email address")
+    private String email;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ tutor-agency/src/main/java/ese/tutoragency/controller/service/SampleService.java
@@ -0,0 +1,11 @@
+package ese.tutoragency.controller.service;
+
+
+import ese.tutoragency.controller.pojos.SignupForm;
+
+
+public interface SampleService {
+
+    public SignupForm saveFrom(SignupForm signupForm);
+
+}
