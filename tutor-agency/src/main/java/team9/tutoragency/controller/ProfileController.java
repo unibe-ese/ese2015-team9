18c587c8dfa3a969a3be3470621a427d3bf11807
laeri <lars.wuethrich@hotmail.com>
2015-10-24 15:07:53 +0200

    tutor-agency/profile shows basic member information for a user
    -if the user is not logged in another page is displayed that he has to log
    in(not-logged-in.jsp) will be updated for some actions to take if this happens
    -if the user is logged in, the profile whill show firstname, lastname, username, email

---
+++ tutor-agency/src/main/java/team9/tutoragency/controller/ProfileController.java
@@ -0,0 +1,47 @@
+package team9.tutoragency.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import team9.tutoragency.controller.exceptions.IllegalDatabaseStateException;
+import team9.tutoragency.model.Member;
+import team9.tutoragency.model.dao.MemberDao;
+
+@Controller
+public class ProfileController {
+
+	@Autowired
+	MemberDao memberDao;
+
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public ModelAndView register(HttpServletResponse response) throws IOException {
+		ModelAndView profile = new ModelAndView("profile");
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+		if (username == null) {
+			return new ModelAndView("not-logged-in");
+		}
+		List<Member> members = memberDao.findByUsername(username);
+		
+		if (members.size() == 0) {
+			return new ModelAndView("not-logged-in");
+		}
+
+		if (members.size() > 1) {
+			throw new IllegalDatabaseStateException("More than one user registered with the username " + username);
+		}
+		profile.addObject("member", members.get(0));
+
+		return profile;
+	}
+}
---
+++ tutor-agency/src/main/java/team9/tutoragency/controller/exceptions/IllegalDatabaseStateException.java
@@ -0,0 +1,8 @@
+package team9.tutoragency.controller.exceptions;
+
+public class IllegalDatabaseStateException extends RuntimeException {
+
+	public IllegalDatabaseStateException(String s) {
+        super(s);
+    }
+}
