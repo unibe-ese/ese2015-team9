ecafcb6cbc40213d136628ab9d079b4e1bf3dabc
brnsnt <brunosenti@gmail.com>
2015-11-06 13:33:25 +0100

    Revert "SearchForm"
    This reverts commit f16d84ce65402d14027de69d7960fb5e9fe987e6.

--- tutor-agency/src/main/java/team9/tutoragency/controller/pojos/searchForm.java
+++ tutor-agency/src/main/java/team9/tutoragency/controller/pojos/searchForm.java
@@ -1,6 +1,5 @@
 package team9.tutoragency.controller.pojos;
 
-public class SearchForm {
 import javax.validation.constraints.NotNull;
 
 public class searchForm {
@@ -8,23 +7,12 @@ public class searchForm {
 	
 	private String selectedCourse;
 	
-	private String searchText;
 	private String selectedUniversity;
 	
 	public String getSelectedUniversity() {
 		return selectedUniversity;
 	}
 
-	public String getSearchText() {
-		return searchText;
-	}
-
-	public void setSearchText(String searchText) {
-		this.searchText = searchText;
-	}
-	public String getSelectedCourse() {
-		return selectedCourse;
-	}
 	public void setSelectedUniversity(String chosenUniversity) {
 		this.selectedUniversity = chosenUniversity;
 	}
