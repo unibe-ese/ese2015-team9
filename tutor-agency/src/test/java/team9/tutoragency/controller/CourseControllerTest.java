fa078b4014258f74f98768f0c38e2f835a69e4d7
Simon Curty <simon.curty@students.unibe.ch>
2015-11-15 18:15:08 +0100

    Course-and EditController integration test

--- tutor-agency/src/test/java/team9/tutoragency/controller/CourseControllerTest.java
+++ tutor-agency/src/test/java/team9/tutoragency/controller/CourseControllerTest.java
@@ -11,23 +11,43 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContextManager;
+import org.springframework.test.context.web.WebAppConfiguration;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import team9.tutoragency.controller.pojos.AddCourseForm;
 import team9.tutoragency.controller.service.CourseService;
 import team9.tutoragency.model.Course;
 import team9.tutoragency.model.Member;
+ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Test of the class {@link CourseController}. {@link CourseService} provides
  * underlying functionality of {@link CourseController} and is tested separately 
  * in {@link CourseServiceTest}.
+ * 
+ * @author curtys
  */
 @RunWith(MockitoJUnitRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {
+"file:src/main/webapp/WEB-INF/config/springMVC.xml",
+"file:src/main/webapp/WEB-INF/config/springData.xml" })
+@Transactional
+@Rollback
 public class CourseControllerTest {
     
     @Mock
@@ -36,14 +56,25 @@ public class CourseControllerTest {
     private CourseController controller;
     private final Member member = new Member();
     
+    @Autowired private WebApplicationContext wac;
+    private MockMvc mockMvc;
+    
+    private TestContextManager testContextManager;
+    
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        //this allows multiple runners, i.e. MockitoJUnitRunner and SpringJUnit4Runner
+        this.testContextManager = new TestContextManager(getClass());
+        this.testContextManager.prepareTestInstance(this);
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
         
+        // Mocks the security context and returns our member as authenticated user.
         Authentication authentication = Mockito.mock(Authentication.class);
         SecurityContext securityContext = Mockito.mock(SecurityContext.class);
         Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
         SecurityContextHolder.setContext(securityContext);
-        Mockito.when(authentication.getPrincipal()).thenReturn(member);        
+        Mockito.when(authentication.getPrincipal()).thenReturn(member);
     }
 
     /**
@@ -52,9 +83,15 @@ public class CourseControllerTest {
     @Test
     public void testShowAddCourseView() throws Exception {
         HttpServletResponse response = null;
-        ModelAndView expResult = new ModelAndView("addCourse");
+//        ModelAndView expResult = new ModelAndView("addCourse");
         ModelAndView result = controller.showAddCourseView(response);
-        assertEquals(expResult.getViewName(), result.getViewName());
+//        assertEquals(expResult.getViewName(), result.getViewName());
+        assertViewName(result, "addCourse");
+        this.mockMvc.perform(get("/addCourse"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("universities"))
+                .andExpect(model().attributeExists("courses"))
+                .andExpect(model().attributeExists("member"));
     }
 
     /**
@@ -65,9 +102,10 @@ public class CourseControllerTest {
         AddCourseForm addCourseForm = new AddCourseForm();
         BindingResult bResult = null;
         RedirectAttributes redirectAttributes = null;
-        ModelAndView expResult = new ModelAndView("addCourse");
+//        ModelAndView expResult = new ModelAndView("addCourse");
         ModelAndView result = controller.updateDropdown(addCourseForm, bResult, redirectAttributes);
-        assertEquals(expResult.getViewName(), result.getViewName());
+//        assertEquals(expResult.getViewName(), result.getViewName());
+        assertViewName(result, "addCourse");
     }
 
     /**
--- tutor-agency/src/test/java/team9/tutoragency/controller/EditControllerTest.java
+++ tutor-agency/src/test/java/team9/tutoragency/controller/EditControllerTest.java
@@ -1,60 +1,63 @@
 package team9.tutoragency.controller;
 
-import org.hamcrest.Matcher;
-import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import org.junit.Before;
-import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.ModelAndView;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
 import team9.tutoragency.controller.service.EditFormValidationService;
 import team9.tutoragency.controller.service.MemberService;
 import team9.tutoragency.controller.service.UniversityAccessService;
 import team9.tutoragency.model.Member;
 import team9.tutoragency.model.University;
 
-import javax.servlet.http.HttpServletResponse;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContextManager;
+import org.springframework.test.context.web.WebAppConfiguration;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DirectFieldBindingResult;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import team9.tutoragency.controller.pojos.EditForm;
-import team9.tutoragency.controller.pojos.SignupForm;
-import team9.tutoragency.controller.service.SignupFormValidationService;
-import team9.tutoragency.controller.service.SignupFromSaveService;
+
+ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 /**
- * For know this Class test only the Edit method.
+ * Test of the class {@link EditController}.
+ * 
  * @author brn
- *
+ * @author curtys
  */
 @RunWith(MockitoJUnitRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {
+"file:src/main/webapp/WEB-INF/config/springMVC.xml",
+"file:src/main/webapp/WEB-INF/config/springData.xml" })
+@Transactional
+@Rollback
 public class EditControllerTest {
-	private MockMvc mockMvc;
+    
 	private Member member ;
 	private Member editedMember ;
 	private University uni1;
@@ -63,20 +66,33 @@ public class EditControllerTest {
 	private List<String> uniNames;
 	
 	@Mock
-	EditFormValidationService validator;
+	private EditFormValidationService validator;
 	@Mock
-	MemberService memberService;
+	private MemberService memberService;
 	@Mock
-	UniversityAccessService uniService;
+	private UniversityAccessService uniService;
 	
 	@InjectMocks
-	EditController controller;
+	private EditController controller;
+    @Captor
+    private ArgumentCaptor<EditForm> captor;
 	
+    @Autowired private WebApplicationContext wac;
+    private MockMvc mockMvc;
+    private TestContextManager testContextManager;
 	
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
+        
+        //this allows multiple runners, i.e. MockitoJUnitRunner and SpringJUnit4Runner
+        this.testContextManager = new TestContextManager(getClass());
+        this.testContextManager.prepareTestInstance(this);
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+        
 		//initialize test beans
 		member = new Member("firstname", "lastname", "name@mail.com", "username", "password" );
+        member.setId(Long.valueOf(1));
 		editedMember = new Member("firstname2", "lastname2", "name2@mail.com", "username2", "password2" );
 		uni1 = new University();
 		uni2 = new University();
@@ -84,7 +100,19 @@ public class EditControllerTest {
 		uni2.setName("name2");
 		universities = Arrays.asList(new University[]{uni1, uni2});
 		uniNames = Arrays.asList(new String[]{"name1", "name2"});
-		this.mockMvc = MockMvcBuilders.standaloneSetup(new EditController()).alwaysExpect(status().isOk()).build();
+        
+        
+
+        
+        //Mock the SecurityContext and Authentication
+		Authentication authentication = Mockito.mock(Authentication.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(authentication.getPrincipal()).thenReturn(member);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        
+        Mockito.when(uniService.findAll()).thenReturn(universities);
+//        Mockito.doNothing().when(memberService).saveEditChange(any(Member.class), captor.capture());
 	}
 	@Test
 	public void testExtractNames(){
@@ -93,25 +121,52 @@ public class EditControllerTest {
 	}
 	
 	@Test
-	public void testEdit(){	
-		//Mock the SecurityContext and Authentication
-		Authentication authentication = Mockito.mock(Authentication.class);
-        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
-        when(authentication.getPrincipal()).thenReturn(member);
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        SecurityContextHolder.setContext(securityContext);
-      
-        when(uniService.findAll()).thenReturn(universities);
+	public void testEdit() throws Exception{  
         EditForm form = new EditForm(member);
         form.setUniversities(uniNames);
         
-        try {
-			mockMvc.perform(get("/edit")).andExpect(forwardedUrl("edit")).andExpect(model().attribute("universityChoices", uniNames)).andExpect(model().attribute("editForm", form)).andExpect(model().attribute("member", member));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
+        ModelAndView result = controller.edit();
+        assertViewName(result, "edit");
+        mockMvc.perform(get("/edit"))
+                .andExpect(forwardedUrl("/pages/edit.jsp"))
+                .andExpect(model().attributeExists("editForm"))
+                .andExpect(model().attribute("member", member))
+                .andExpect(model().attributeExists("universityChoices"));
          
 	}
+
+    /**
+     * Test of saveChange method, of class {@link EditController}, when errors are present.
+     */
+    @Test
+    public void testSaveChangeErrors() throws Exception {
+        EditForm editForm = null;
+        BindingResult error = new DirectFieldBindingResult(editForm, "editForm");
+        error.reject("test error");
+        RedirectAttributes redirectAttributes = null;
+        ModelAndView expResult = new ModelAndView("edit");
+        expResult.addObject("universityChoices", uniNames);
+        expResult.addObject("editForm", editForm);
+        expResult.addObject("member", member);
+        ModelAndView result = controller.saveChange(editForm, error, redirectAttributes);
+        assertViewName(result, "edit");
+        assertEquals(expResult.getModelMap(), result.getModelMap());
+
+    }
+    
+    /**
+     * Test of saveChange method, of class {@link EditController}, when no errors are present.
+     */
+    @Test
+    public void testSaveChangeNoErrors() throws Exception {
+        EditForm editForm = new EditForm(member);
+        BindingResult error = new DirectFieldBindingResult(editForm, "editForm");
+        RedirectAttributes redirectAttributes = null;
+        ModelAndView expResult = new ModelAndView("profile");
+        expResult.addObject("member", member);
+        expResult.addObject("unis", null);
+        ModelAndView result = controller.saveChange(editForm, error, redirectAttributes);
+        assertViewName(result, "profile");
+        assertEquals(expResult.getModelMap(), result.getModelMap());
+    }
 }
