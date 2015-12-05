de964f7b24f4001e29ce2b782296b5c30b54a7a2
Simon Curty <simon.curty@students.unibe.ch>
2015-12-05 11:57:51 +0100

    Page scrolls again to the right position when selecting the request notification.

--- tutor-agency/src/main/webapp/js/profile.js
+++ tutor-agency/src/main/webapp/js/profile.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     var res = $(location).attr('href').split('#');
     if(res[res.length-1] === 'offers') {
+        $('#offer-seg').addClass('.notransition');
         $('#checkbox-offer').prop('checked', true);
+        $('html, body').animate({
+            scrollTop: $("#offers").offset().top
+        }, 800);
     }
 });
